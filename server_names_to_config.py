0bf91308ee65a23a659df334eb838e53abd26a44
Zacharie Bedecarrax <zbedecarrax@emi.u-bordeaux.fr>
2022-11-07 15:18:52 +0100

    adding username choice

--- server_names_to_config.py
+++ server_names_to_config.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
     try:
+        username = input("Enter username: ")
         with open('/Users/benoitboidin/Desktop/cremi_server_names.txt') as input_file:
             lines = input_file.readlines()
             with open('cremi_config.txt', 'w+') as output_file:
                 for name in lines:
                     name = name.replace('\n', '')
                     print('Adding {} to config file.'.format(name))
-                    output_file.write('Host' + name + '\n  HostName'+ name + '.emi.u-bordeaux.fr\n  User bboidin\n  ProxyJump cremi\n  ForwardX11 yes\n\n')
-
+                    output_file.write('Host' + name + '\n  HostName'+ name + '.emi.u-bordeaux.fr\n  User ' + username' +  '\n  ProxyJump cremi\n  ForwardX11 yes\n\n')
     except TypeError:
         print("Error")
